e65098edeb9e1f5245535625eee949d8ec9e2715
Robert <RobKomar98@gmail.com>
2024-04-14 03:54:50 +0200

    feat: Refactor vector store and milvus store for hybrid search
    Updated vectorstore and milvus_store to support hybrid search. This includes changes in the way data is prepared and inserted, as well as the search function itself. Also, improved the handling of CSV file paths and added more detailed logging.

--- vectorstore/csv_loader.py
+++ vectorstore/csv_loader.py
@@ -1,16 +1,21 @@
-from random import random
-import pandas as pd
+from typing import List
+import os
 import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
+from typing import List
+
+import dotenv
+import pandas as pd
 from langchain_openai import OpenAIEmbeddings
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from openai import AzureOpenAI
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
 from pymilvus import DataType, MilvusClient
 from pinecone_text.sparse import BM25Encoder
-from raptor import Raptor
-import statistics
+# from raptor import Raptor
+
 
 class EmbeddingGenerator:
     """Generates embeddings for textual data."""
@@ -18,44 +23,50 @@ class EmbeddingGenerator:
     def __init__(self, dimension=1536):
         self.dimension = dimension
         self.emebeding_model = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
-    
+
+        self.azure_openai_client = AzureOpenAI(api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+                                               api_version="2024-02-15-preview",
+                                               azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), )
 
     def generate(self, text):
         """Generates an embedding for the text."""
         return self.emebeding_model.embed_query(text=text)
 
+    def create_embedding(self, text_to_embed: str, model: str = "embeds-hackathon") -> List[float]:
+        embed: List[float] = (
+            self.azure_openai_client.embeddings.create(input=[text_to_embed], model=model).data[0].embedding)
+        return embed
+
 
 class CSVLoader:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.client = "" #MilvusClient(uri="http://localhost:19530")
+        self.client = MilvusClient(uri="http://localhost:19530")
         self.bm25 = BM25Encoder()
-        self.raptor = Raptor()
+        # self.raptor = Raptor()
         self.data = self.load_data()
+        self.embed_gen = EmbeddingGenerator()
 
     def load_data(self):
         data = pd.read_csv(self.file_path)
         # data = data[data["cat_level_2"] == "Laptopy"]
-        print (data.shape, "shape")
-        data = data.head(20)
-        data_descriptions = data["long_description"].values.tolist()
+        data_descriptions = data["desc"].values.tolist()
         # data_categories = data["cat_level_2"].values.tolist()
 
-        #when there is a missing value in the description
+        # when there is a missing value in the description
         self.fillna_type_compliant(data)
 
-        #CHECK DATA LENGTH
-        filtered_descriptions = [x for x in data_descriptions if isinstance(x, str)]
-
-        #RAPTOR THINGS
+        # CHECK DATA LENGTH
+        # filtered_descriptions = [x for x in data_descriptions if isinstance(x, str)]
+        # RAPTOR THINGS
         # docs_length = [self.raptor.num_of_tokens_for_doc(desc,"gpt-4") for desc in filtered_descriptions]
         # print("Descriptions length: ",docs_length, "\nMediana: ",statistics.median(docs_length), "\nWartość średnia: ",statistics.mean(docs_length), "\nMax: ",max(docs_length), "\nMin: ",min(docs_length))
         # raptor_desriptions_tree = self.raptor.recursive_embed_cluster_summarize(filtered_descriptions)
         # print("Wynik: ", raptor_desriptions_tree)
 
-        #FIT BM25
-        self.bm25.fit(data['long_description'].values.tolist())
-
+        # FIT BM25
+        self.bm25.fit(data_descriptions)
+        print("Dane załadowane: ", data.head(5))
         return data
 
     @staticmethod
@@ -78,29 +89,25 @@ class CSVLoader:
         schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
         schema.add_field(field_name="embedding", datatype=DataType.FLOAT_VECTOR, dim=1536)
         return schema
+
     @staticmethod
     def _prepare_index():
         index_params = MilvusClient.prepare_index_params()
         index_params.add_index(field_name="embedding", index_type="AUTOINDEX", metric_type="L2")
         return index_params
-    #------------------
+
+    # ------------------
     def bm25_encode(self, text):
         list_sparse = self.bm25.encode_documents(text)
         dict_sparse = dict(zip(list_sparse['indices'], list_sparse['values']))
         # print("dict sparse: ",dict_sparse)
         return dict_sparse
-    
-    def prepare_data(
-        self,
-        embedding_generator: EmbeddingGenerator = EmbeddingGenerator(),
-        embedding_field_name="long_description",
-        exclude_fields=None,
-        hybrid  = False
-    ):
+
+    def prepare_data(self, embedding_field_name="desc", exclude_fields=None, hybrid=False):
         if exclude_fields is None:
             exclude_fields = ["id"]
 
-        print ("data in prepare ds: ", self.data.keys())
+        print("data in prepare ds: ", self.data.keys())
 
         data_rows = []
         for _, row in self.data.iterrows():
@@ -109,8 +116,8 @@ class CSVLoader:
             for column in self.data.columns:
                 if column not in exclude_fields:
                     if column == embedding_field_name:
-                        prepared_row["embedding"] = embedding_generator.generate(row[column])
-                        if(hybrid): prepared_row["sparse_vector"] = self.bm25_encode(row[column])
+                        prepared_row["embedding"] = self.embed_gen.create_embedding(row[column])
+                        if (hybrid): prepared_row["sparse_vector"] = self.bm25_encode(row[column])
                     else:
                         prepared_row[column] = row[column]
             data_rows.append(prepared_row)
@@ -119,31 +126,23 @@ class CSVLoader:
         # print("END: ",data_rows[0]["sparse_vector"])
         return data_rows
 
-
-
-    def process_row(self,row, embedding_generator, embedding_field_name, exclude_fields):
+    def process_row(self, row, embedding_field_name, exclude_fields):
         prepared_row = {}
         for column in row.index:
             if column not in exclude_fields:
                 if column == embedding_field_name:
-                    prepared_row["embedding"] = embedding_generator.generate(row[column])
+                    prepared_row["embedding"] = self.embed_gen.create_embedding(row[column])
                     prepared_row[column] = row[column]
                 else:
                     prepared_row[column] = row[column]
         return prepared_row
 
-    def insert_data(self,client, collection_name, data,i):
-        print(f"Inserting data: ",i)
+    def insert_data(self, client, collection_name, data, i):
+        print(f"Inserting data: ", i)
         client.insert(collection_name=collection_name, data=data)
 
-    def vector_store_creation(
-        self,
-        embedding_generator: EmbeddingGenerator = EmbeddingGenerator(),
-        embedding_field_name="long_description",
-        exclude_fields=None,
-        collection_name=None,
-        num_threads=7
-    ):
+    def vector_store_creation(self, embedding_generator: EmbeddingGenerator = EmbeddingGenerator(),
+                              embedding_field_name="desc", exclude_fields=None, collection_name=None, num_threads=7):
         assert collection_name is not None, "Collection name must be provided"
         if exclude_fields is None:
             exclude_fields = ["id"]
@@ -152,44 +151,39 @@ class CSVLoader:
         def visualize_thread_operation(operation, item):
             thread_id = threading.get_ident()  # Gets the current thread's identifier
             print(f"{threading.current_thread().name} (ID: {thread_id}): {operation} {item}")
+
         # POD DODANIE 100k rekordów z zabezpieczeniem na szybko
-        self.client.create_collection(
-            collection_name=collection_name,
-            schema=self._prepare_schema(),
-            index_params=self._prepare_index(),
-        )
-        #------------------
+        self.client.create_collection(collection_name=collection_name, schema=self._prepare_schema(),
+                                      index_params=self._prepare_index(), )
+        # ------------------
         with ThreadPoolExecutor(max_workers=num_threads) as executor:
             # Dispatch phase
-            result=[]
-            futures = [executor.submit(self.process_row, row, embedding_generator, embedding_field_name, exclude_fields) 
-                       for chunk in [self.data[i:i+1000] for i in range(0, len(self.data), 1000)] 
-                       for _, row in chunk.iterrows()]
-            
+            result = []
+            futures = [executor.submit(self.process_row, row, embedding_field_name, exclude_fields) for chunk in
+                       [self.data[i:i + 1000] for i in range(0, len(self.data), 1000)] for _, row in chunk.iterrows()]
+
             # Collection phase
             start = time.time()
             for i, future in enumerate(futures):
-                result.append(future.result())  
-                if i % 1000 == 0:  
+                result.append(future.result())
+                if i % 1000 == 0:
                     visualize_thread_operation("processing next chunk ", i // 1000 + 1)
                     self.insert_data(self.client, collection_name, result, i // 1000 + 1)
-                    result=[]
+                    result = []
                     end = time.time()
-                    print("Czas na chunk: ", end-start)
+                    print("Czas na chunk: ", end - start)
             if result:
                 visualize_thread_operation("processing final chunk ", "...")
                 self.insert_data(self.client, collection_name, result, "final")
 
-        return 1    
+        return 1
 
-    
 
 if __name__ == "__main__":
     print("Running the CSVLoader")
-    csv_loader = CSVLoader("data/products_all.csv")
+    csv_loader = CSVLoader("/Users/bartek/Documents/ai_persp/accenture-hackathon/backend/data/products.csv")
     prepared_data = csv_loader.prepare_data()
     # csv_loader.load_data()
     # csv_loader.vector_store_creation(collection_name="full_morele_pl")
     # Fix the printing function to nicely format the output
-    import json
-    # print(json.dumps(prepared_data, indent=2))
+
--- vectorstore/milvus_store.py
+++ vectorstore/milvus_store.py
@@ -1,68 +1,59 @@
 import logging
+import os
 import time
 from random import random
 
 from pymilvus import DataType, MilvusClient, Collection, AnnSearchRequest, RRFRanker, connections
 
-
 from csv_loader import EmbeddingGenerator
 from csv_loader import CSVLoader
-
+from setup import ROOT_FOLDER_NAME
 logging.basicConfig(level=logging.INFO)
 
 
 class MilvusStoreWithClient:
-    def __init__(self, client_uri: str = "http://localhost:19530", csv_file_path: str = "data/products.csv"):
+    def __init__(self, client_uri: str = "http://localhost:19530",
+                 csv_file_path: str = r'\backend\data\products.csv'):
+        project_root = os.getcwd()
+
+        while os.path.basename(project_root) != ROOT_FOLDER_NAME:
+            project_root = os.path.dirname(project_root)
+            if os.path.basename(project_root) == '/':  # We've reached the file system root without finding the folder
+                raise FileNotFoundError(f"The root folder '{ROOT_FOLDER_NAME}' was not found in the directory tree.")
+
+        csv_path = project_root + csv_file_path
+
         self.client = MilvusClient(uri=client_uri)
-        self.csv_loader = CSVLoader(csv_file_path)
+        self.csv_loader = CSVLoader(csv_path)
         self.logger = logging.getLogger(__name__)
 
+    # DO HYBRID SEARCH
     @staticmethod
     def _prepare_schema():
         schema = MilvusClient.create_schema(auto_id=True, enable_dynamic_field=True)
         schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
         schema.add_field(field_name="embedding", datatype=DataType.FLOAT_VECTOR, dim=1536)
+        schema.add_field(field_name="sparse_vector", datatype=DataType.SPARSE_FLOAT_VECTOR)
         return schema
+
     @staticmethod
     def _prepare_index():
         index_params = MilvusClient.prepare_index_params()
-        index_params.add_index(field_name="embedding", index_type="AUTOINDEX", metric_type="L2")
-        return index_params
-    def make_collection(self, collection_name: str):
-        self.client.create_collection(
-            collection_name=collection_name,
-            schema=self._prepare_schema(),
-            index_params=self._prepare_index(),
-        )
-
-
-    #DO HYBRID SEARCH
-    @staticmethod
-    def _prepare_schema_kozak():
-        schema = MilvusClient.create_schema(auto_id=True, enable_dynamic_field=True)
-        schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
-        schema.add_field(field_name="embedding", datatype=DataType.FLOAT_VECTOR, dim=1536)
-        schema.add_field(field_name="sparse_vector",  datatype=DataType.SPARSE_FLOAT_VECTOR)
-        return schema
-    @staticmethod
-    def _prepare_index_kozak():
-        index_params = MilvusClient.prepare_index_params()
-        index_params.add_index(field_name="embedding", index_type="AUTOINDEX", metric_type="L2")
-        index_params.add_index(field_name="sparse_vector",index_name="sparse_inverted_index", index_type="SPARSE_INVERTED_INDEX", metric_type="IP", params={"drop_ratio_build": 0.2}) #Inner Product
+        index_params.add_index(field_name="embedding", index_type="AUTOINDEX", metric_type="COSINE")
+        index_params.add_index(field_name="sparse_vector", index_name="sparse_inverted_index",
+                               index_type="SPARSE_INVERTED_INDEX", metric_type="IP",
+                               params={"drop_ratio_build": 0.2})  # Inner Product
         return index_params
 
     def describe_collection(self, collection_name: str):
-        return (self.client.describe_collection(collection_name),self.client.list_collections())
-    
-    def make_collection_kozak(self, collection_name: str):
-        self.client.create_collection(
-            collection_name=collection_name,
-            schema=self._prepare_schema_kozak(),
-            index_params=self._prepare_index_kozak(),
-        )
-        return
-    # ===================
+        return (self.client.describe_collection(collection_name), self.client.list_collections())
 
+    def make_collection(self, collection_name: str):
+        self.client.create_collection(collection_name=collection_name, schema=self._prepare_schema(),
+            index_params=self._prepare_index(), )
+        return
+
+    # ===================
 
     def recreate_collection(self, collection_name: str):
         if self.client.has_collection(collection_name):
@@ -74,96 +65,57 @@ class MilvusStoreWithClient:
         logging.info(f"Creating collection: {collection_name}")
         self.make_collection(collection_name)
 
-    def insert_data_from_csv(self, collection_name: str, hybrid:bool = False):
+    def insert_data_from_csv(self, collection_name: str, hybrid: bool = False):
         prepared_data = self.csv_loader.prepare_data(hybrid=hybrid)
         self.logger.info(f"Inserting {len(prepared_data)} records into collection")
         self.client.insert(collection_name=collection_name, data=prepared_data)
         time.sleep(5000)
-        return 
+        return
 
-    def search(
-        self,
-        collection_name: str,
-        query: list = None,
-        fixed_filter: str = None,
-        limit: int = 100,
-        output_fields: list = None,
-        search_params: dict = None,
-    ):
+    def search(self, collection_name: str, query: list = None, fixed_filter: str = None, limit: int = 100,
+            output_fields: list = None, search_params: dict = None, ):
         assert query is not None, "Query must be provided"
         data = EmbeddingGenerator().generate(query)
         if output_fields is None:
-            output_fields = ["id", "price","cat_level_4", "cat_level_5", "url","specification","all_variants"]
+            output_fields = ["id", "price", "cat_level_4", "cat_level_5", "url", "specification", "all_variants"]
 
-        
-        return self.client.search(
-            collection_name=collection_name,
-            data=[data],
-            filter=fixed_filter,
-            limit=limit,
-            output_fields=output_fields,
-            # search_params=search_params,
+        return self.client.search(collection_name=collection_name, data=[data], filter=fixed_filter, limit=limit,
+            output_fields=output_fields, # search_params=search_params,
         )
 
-    def hybrid_search(self,        
-        collection_name: str,
-        query: list = None,
-        fixed_filter: str = None,
-        limit: int = 2,
-        output_fields: list = None):
+    def hybrid_search(self, collection_name: str, query: list = None, fixed_filter: str = 'access==0', limit: int = 1,
+                      output_fields: list = ['schema', 'neighbours', 'schema_name', 'table_name', 'constraints']):
 
         connections.connect(alias="default")
         collection = Collection(name=collection_name)
         # collection.load()
         # print(len(EmbeddingGenerator().generate(query)))
-        res = collection.hybrid_search(
-            output_fields=["id", "price","cat_level_4", "cat_level_5", "url","specification","all_variants"],
-            reqs=[
-                AnnSearchRequest(
-                    data=[EmbeddingGenerator().generate(query)], ## Dense vectors
-                    anns_field="embedding", # Field name of the vectors
-                    param={"metric_type": "L2"},
-                    limit=limit,
-                ),
-                AnnSearchRequest(
-                    data=[self.csv_loader.bm25_encode(query)], ## Sparse vector
-                    anns_field="sparse_vector", # Field name of the vectors
-                    param={"metric_type": "IP", "params": {"drop_ratio_search": 0.2,}},
-                    limit=limit,
-                )
-            ],
-            rerank=RRFRanker(),
-            limit = limit
-        )
+        res = collection.hybrid_search(output_fields=output_fields, filter=fixed_filter,
+            reqs=[AnnSearchRequest(data=[EmbeddingGenerator().generate(query)],  ## Dense vectors
+                anns_field="embedding",  # Field name of the vectors
+                param={"metric_type": "COSINE"}, limit=limit, ),
+                AnnSearchRequest(data=[self.csv_loader.bm25_encode(query)],  ## Sparse vector
+                    anns_field="sparse_vector",  # Field name of the vectors
+                    param={"metric_type": "IP", "params": {"drop_ratio_search": 0.2, }}, limit=limit, )],
+            rerank=RRFRanker(), limit=limit)
 
-        print("Odpowiedź hybrid search: ", res)
-        return res
+        print(res[0][0].entity.get('schema_name'))
+
+        return {"SCHEMA_NAME": res[0][0].entity.get('schema_name'), "TABLE_NAME": res[0][0].entity.get('table_name'),
+                "TABLE_SCHEMA": res[0][0].entity.get("schema"),
+                "IDS OF TABLES THAT ARE RELATED WITH GIVEN TABLE": res[0][0].entity.get('neighbours'),
+                "TABLE CONSTRAINTS": res[0][0].entity.get('constraints')}
 
 
 if __name__ == "__main__":
     # creating collection
-    COLLECTION_NAME_HYBRID = "hybrid40_morele_pl"
-    COLLECTION_NAME = "new_morele_pl"
-    milvus_store = MilvusStoreWithClient(csv_file_path="data/products.csv")
-    # use only when you want to create a new collection with the same name (data clearing)
-    # milvus_store.recreate_collection(COLLECTION_NAME)
-    # use otherwise
-    # | for different websites it would be good idea to create a new collection for each website
+    COLLECTION_NAME = "tests"
+    milvus_store = MilvusStoreWithClient(
+        csv_file_path="/Users/bartek/Documents/ai_persp/accenture-hackathon/backend/data/products.csv")
+
+    # HYBRYDA
     # milvus_store.make_collection(COLLECTION_NAME)
-    # insert new data but be careful to no create too many duplicates
-    # milvus_store.insert_data_from_csv(COLLECTION_NAME)
+    # milvus_store.insert_data_from_csv(COLLECTION_NAME, hybrid=True)
+    searched_values = milvus_store.hybrid_search(COLLECTION_NAME, query="Jakie są produkty?")
 
-
-    #KLASYCZNA
-    # milvus_store.make_collection(COLLECTION_NAME)
-    # milvus_store.insert_data_from_csv(COLLECTION_NAME, hybrid=False)
-    # searched_values = milvus_store.search(COLLECTION_NAME, query="szukam zabawek dla dziecka")
-    # print( "searched values: ", searched_values)
-
-    #HYBRYDA
-    # milvus_store.make_collection_kozak(COLLECTION_NAME_HYBRID)   
-    # milvus_store.insert_data_from_csv(COLLECTION_NAME_HYBRID, hybrid=True)
-    searched_values = milvus_store.hybrid_search(COLLECTION_NAME_HYBRID, query="szukam zabawek dla dziecka")
-    print( "searched values: ", searched_values)
-
-    # print(milvus_store.describe_collection(COLLECTION_NAME_HYBRID))
+    print("WYNIK: ", searched_values)
