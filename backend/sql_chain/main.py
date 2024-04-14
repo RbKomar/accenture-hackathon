3b27934d626b581ca331949d9b91d7321428d0a6
bartoszkozakiewicz <bartosz040824@gmail.com>
2024-04-14 02:46:49 +0200

    agent

--- backend/sql_chain/main.py
+++ backend/sql_chain/main.py
@@ -1,3 +1,9 @@
+import sys
+
+from openai import AzureOpenAI
+sys.path.append("../../src/llm/")
+sys.path.append("../../src/milvus_next/")
+
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
@@ -6,38 +12,59 @@ from langchain_core.output_parsers import StrOutputParser
 
 import os
 import load_dotenv
+load_dotenv.load_dotenv("/Users/bartek/Documents/ai_persp/accenture-hackathon/src/.env")
+from langchain_openai import AzureChatOpenAI
 
-load_dotenv.load_dotenv()
+
+
+from llm_manager import LLMManager
+from milvus_store import MilvusStoreWithClient
 #----------------------------------------------------------
 # passw = os.environ.get("SQL_PASS")
 class sqlAgent():
-    def __init__(self,uri:str):
-        self.db = SQLDatabase.from_uri(uri)
+    def __init__(self,collection_name:str):
+        # self.llm = AzureChatOpenAI(
+        #     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        #     api_version="2024-02-15-preview",
+        #     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        # )
+        self.llm = ChatOpenAI()
+
+        self.milvus_store = MilvusStoreWithClient()
+        self.db_name = 'hackaton-gr9-sqldb'
+        self.COLLECTION_NAME =collection_name  
         self.schema = None
-        self.query = None
-        self.uri = uri
+        self.query = 'Tell me about products.'
        
     #     self.llm = ChatOpenAI()
-    #     self.prompt = ChatPromptTemplate("""Based on the table schema below, write a SQL query that would answer the user's question:
-    #     {schema}
+        self.prompt = ChatPromptTemplate.from_template("""Based on the table schema name, table name, schema below,  tabels related, constraints write a SQL query that would answer the user's question:
+        {schema}
 
-    #     Question: {question}
-    #     SQL Query:""")
-    #     self.prompt2 = ChatPromptTemplate("""Based on the table schema below, question, sql query, and sql response, write a natural language response:
-    #     {schema}
+        Question: {question}
+        Return only sql query, nothing all.
+        You always have to add [hackaton-gr9-sqldb]. before schema name, when you select from that schema, for example: SELECT * FROM [hackaton-gr9-sqldb].SalesLT.vGetAllCategories
+        where in this example name of the schema is SalesLT.     
+                                                                                  
+        SQL Query:""")
+        self.prompt2 = ChatPromptTemplate.from_template("""Based on the table schema below, question, sql query, and sql response, write a natural language response:
+        {schema}
 
-    #     Question: {question}
-    #     SQL Query: {query}
-    #     SQL Response: {response}""")
+        Question: {question}
+        SQL Query: {query}
+        SQL Response: {response}""")
         
+
     # # ----------- PART 1 (Get query to ask database)
-    # def get_db_schema(self):
-    #     self.db = SQLDatabase.from_uri(self.uri)
-    #     self.schema = self.db.get_table_info()
-    #     return self.schema
-    # def schema_chain(self):
-    #     chain = RunnablePassthrough.assign(schema=self.get_db_schema) | self.prompt | self.llm | StrOutputParser()
-    #     return chain
+    def vs_search(self, *args, **kwargs):
+        print("Received args:", args)
+        print("Received kwargs:", kwargs)
+        schema = self.milvus_store.hybrid_search(collection_name=self.COLLECTION_NAME, query=self.query)
+        print("searched schema", schema)
+        return schema
+
+    def schema_chain(self):
+        chain = RunnablePassthrough.assign(schema=self.vs_search) | self.prompt | self.llm | StrOutputParser()
+        return chain
     # # ----------- 
 
     # # ----------- PART 2 (Get all in one)
@@ -48,16 +75,14 @@ class sqlAgent():
     #     chain = RunnablePassthrough.assign(query = self.schema_chain).assign(schema=self.get_db_schema, response = lambda vars: self.run_query(vars["query"])) | self.prompt2 | self.llm
     #     return chain
     
-    def test(self):
-        return self.db.run("SELECT * FROM Album LIMIT 5")
+    
 
     
 if __name__ == "__main__":
-    user_question = 'how many albums are there in the database?'
-    agent = sqlAgent(f"mysql+mysqlconnector://root:{"1234"}@localhost:3306/Chinook")
-
-    # agent.schema_chain().invoke({"question": user_question})
+    user_question = 'Tell me about products.'
+    agent = sqlAgent(collection_name="tests")
+    # agent.vs_search()
+    answer = agent.schema_chain().invoke({"question": user_question})
+    print("answer: ", answer)
     # agent.full_chain().invoke({"question": user_question})
-    print(agent.test())
-#show variables like port
-#mysql -u root -p
+    
